b718984fb15df24a67a3975ecda2996586500b58
Nicholas Draper <nick@WL-203-244.WPA.Claremont.Edu>
2019-03-11 15:30:25 -0700

    changed location

--- Clinic/Proj1/e_field.m
+++
@@ -1,4 +0,0 @@
-function [e_field_x,e_field_y] = e_field(result,x,y)
-    [e_field_x,e_field_y] = evaluateGradient(result,x,y);
-end
-
--- Clinic/Proj1/q_calc.m
+++
@@ -1,6 +0,0 @@
-function q = q_calc(result,x,y,delta)
-    global sigma;
-    [e_field_x, e_field_y] = e_field(result,x,y);
-    e_field_sq = e_field_x.^2 + e_field_y.^2;
-    q = sigma*e_field_sq.*delta(x,y)';
-end
