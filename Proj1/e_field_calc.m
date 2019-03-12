8fa4c6d17308d4afdd42797ad4c35ec1f1d90e90
Nicholas Draper <nick@WL-203-244.WPA.Claremont.Edu>
2019-03-11 18:24:55 -0700

    updated e_field_calc

--- Proj1/e_field_calc.m
+++ Proj1/e_field_calc.m
@@ -1,4 +1,6 @@
-function elec_field = e_field_calc(result,x,y)
+function e_field = e_field_calc(result,x,y)
     [e_field_x,e_field_y] = evaluateGradient(result,x,y);
+    e_field.x = e_field_x;
+    e_field.y = e_field_y;
 end
 
